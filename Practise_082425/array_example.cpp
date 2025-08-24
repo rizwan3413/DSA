5744a7c99d17842c85be6020dffba73032f47c64
rizwan3413 <rizwan.shaikahmed@gmail.com>
2025-08-24 14:04:54 -0500

    array exmaple

---
+++ Practise_082425/array_example.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<list>
+using namespace std;
+
+class Graph{
+    public:
+    int V;
+    std::list<int> *l;
+    
+    Graph(int V){
+      this->V = V;
+      l = new list<int>[V];
+    }
+    
+    void addEdge(int u, int v){
+      l[u].push_back(v);
+      l[v].push_back(u);
+    }
+    
+    void printAdjList(){
+      for(int i = 0; i< V; i++){
+        cout << i << " : ";
+        for (int neigh : l[i]){
+            cout << neigh << " ";
+        }
+        cout << endl;
+      }
+    }
+};
+
+
+        /*
+        1---------0
+      /   \ 
+    2 ---- 3
+    |
+    4
+*/
+int main()
+{
+    // Graph g(5);
+    // g.addEdge(0,1);
+    // g.addEdge(1,2);
+    // g.addEdge(1,3);
+    // g.addEdge(2,3);
+    // g.addEdge(2,4);
+    // g.printAdjList();
+    
+    int *arr = new int();
+    
+    std::cout << *arr << std::endl;
+    
+    delete arr;
+    
+    arr = new int[5]();
+    
+    // Assign Values values
+    for (int i = 0; i < 5; i++) {
+        arr[i] = i+1;
+    }
+    
+    // Print values
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr; // free array
+    return 0;
+}
